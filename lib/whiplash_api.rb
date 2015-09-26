086a1d9143b32030d95be5c26cee1a7ba8205046
James Marks <marks@vgkids.com>
2015-09-26 13:45:27 -0700

    testing connection

--- lib/whiplash_api.rb
+++ lib/whiplash_api.rb
@@ -1,7 +1,7 @@
 require "rubygems"
 require 'active_resource'
 
-require 'whiplash_api/base'
-require 'whiplash_api/item'
-require 'whiplash_api/order'
-require 'whiplash_api/order_item'
+# require 'whiplash_api/base'
+# require 'whiplash_api/item'
+# require 'whiplash_api/order'
+# require 'whiplash_api/order_item'
